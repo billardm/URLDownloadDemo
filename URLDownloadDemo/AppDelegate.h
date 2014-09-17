87d83f7444f12edfb40ffe58466384eb830dea08
billardm <mike@sbillard.org>
2014-09-17 13:43:20 -0700

    Initial Commit

---
+++ URLDownloadDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  URLDownloadDemo
+//
+//  Created by Michael Billard on 9/17/14.
+//  Copyright (c) 2014 Sympathetic Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ URLDownloadDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  URLDownloadDemo
+//
+//  Created by Michael Billard on 9/17/14.
+//  Copyright (c) 2014 Sympathetic Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
